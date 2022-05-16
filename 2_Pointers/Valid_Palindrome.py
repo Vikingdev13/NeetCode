03066681bb7b4f87c866c74cfe69236b5b582e18
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-05-16 16:27:21 -0400

    Update Valid_Palindrome.py

--- 2_Pointers/Valid_Palindrome.py
+++ 2_Pointers/Valid_Palindrome.py
@@ -1,6 +1,5 @@
 """
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
-
 Given a string s, return true if it is a palindrome, or false otherwise
 """
 
@@ -79,5 +78,3 @@ def validPalindrome3(s):
     return True
 
 print(validPalindrome3(s))
-
-
