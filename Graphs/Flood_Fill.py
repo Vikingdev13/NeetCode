cde98d306344bf8b37811d4224b3d47b95a54d09
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-05-17 14:50:14 -0400

    Create Flood_Fill.py

---
+++ Graphs/Flood_Fill.py
@@ -0,0 +1,33 @@
+"""
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
+
+Return the modified image after performing the flood fill
+sr = source row
+sc = source column
+"""
+"""
+Time: 
+Space: 
+"""
+def floodFill(image, sr, sc, newColor):
+    # is the image is none or the image is already the new color, we dont need to do anything
+    if image == None or image[sr][sc] == newColor:
+        return image
+    # else call the function fill recursively
+    self.fill(image, sr, sc, image[sr][sc], newColor)
+    return image
+# DFS
+def fill(image, row, col, initial, newColor):
+    # base case: out of bounds checking
+    if row < 0 or row >= len(image) or col < 0 or col >= len(image[0]) or image[row][col] != initial:
+        return
+    # change the new pixel to the new color
+    image[row][col] = newColor
+    self.fill(image, row+1, col, initial, newColor) # down
+    self.fill(image, row-1, col, initial, newColor) # up
+    self.fill(image, row, col+1, initial, newColor) # right
+    self.fill(image, row, col-1, initial, newColor) # left
