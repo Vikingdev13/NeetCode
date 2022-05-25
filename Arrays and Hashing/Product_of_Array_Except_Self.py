b59a0683fb4e1542c4357deea40ecd27f16febac
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-05-25 15:52:23 -0400

    Update Product_of_Array_Except_Self.py

--- Arrays and Hashing/Product_of_Array_Except_Self.py
+++ Arrays and Hashing/Product_of_Array_Except_Self.py
@@ -3,14 +3,16 @@ Given an integer array nums, return an array answer such that answer[i] is equal
 
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
-You must write an algorithm that runs in O(n) time and without using the division operation
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
 """
 from numpy import product
 
 """
 *** Naive Approach ***
 Time: O(n)
-Space: O(1) - problem states that output array does not count in memoty calculation, otherwise it would be O(n)
+Space: O(1) - problem states that output array does not count in memory calculation, otherwise it would be O(n)
 But problem statement does not want us to use division operation so we must find another way.
 """
 def productExceptSelf(nums):
@@ -29,11 +31,11 @@ print(productExceptSelf(nums))
 
 """
 Optimal Approach
-Time:
-Space:
+Time: O(n)
+Space: O(1) - the output array doesn't count as memory for the context of this problem
 """
 def productExceptSelf2(nums):
-    # fill result array with all 1s
+    # fill result array with all 1s ** could be all 0s too, either one is fine
     result = [1] * len(nums)
     
     prefix = 1
@@ -41,9 +43,11 @@ def productExceptSelf2(nums):
         result[i] = prefix
         prefix *= nums[i]
 
+    # result = [1,1,2,6] up to this point
     postfix = 1
     for i in range(len(nums)-1,-1,-1):
         result[i] *= postfix
         postfix *= nums[i]
     return result
+nums = [1,2,3,4]
 print(productExceptSelf2(nums))
