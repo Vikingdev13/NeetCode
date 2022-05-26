7fa7805bfd097f7f2307187b5ca0381aaf9de14a
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-05-26 11:27:48 -0400

    Update Merge_Intervals.py

--- Intervals/Merge_Intervals.py
+++ Intervals/Merge_Intervals.py
@@ -11,17 +11,21 @@ def mergeIntervals(intervals):
     # lambda function will return the first element of the list. This allows 
     # us to sort the intervals based on the first element in each list
     intervals.sort(key = lambda pair : pair[0])
-    # initialize result with the first interval
+    # initialize result with the first interval after sorting
     result = [intervals[0]]
 
     # iterate through the intervals beginning with the second element, 
     # since we stored the first in result already
     for interval in intervals[1:]:
-        # if the first element of interval, so in [x,y], this is looking at x, is less than 
-        # or equal to the last element of the last interval in result [x,y] which is the y value
-        # compare it to the first element of that interval [x,y] so x and update
-        if interval[0] <= result[-1][1]:
-            result[-1][1] = max(result[-1][1], interval[1])
+        # if the first pair, is less than or equal to the last element of the 
+        # last interval in result [x,y] which is the y value compare it to the 
+        # first element of that interval [x,y] so x, and update
+        # EX: interval[0] = [2,6] and result[-1][1] = [1,3]
+        # if the x value in interval[0] is less than the y value in result[-1][1]
+        # then we merge them to create one interval, e.i [1,6]
+        lastEnd = result[-1][1]
+        if interval[0] <= lastEnd:
+            lastEnd = max(lastEnd, interval[1])
         else:
             # else append the next interval
             result.append(interval)
