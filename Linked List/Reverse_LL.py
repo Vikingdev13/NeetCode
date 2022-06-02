f45d1520d5c3a03b35f6ef0ce2d16ef324b938b6
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-06-02 15:56:43 -0400

    Update Reverse_LL.py

--- Linked List/Reverse_LL.py
+++ Linked List/Reverse_LL.py
@@ -10,11 +10,17 @@ Time: O(n)
 Space: O(1)
 """
 def reverseList(head):
+    # Initialize 2 pointers, prev will initialize to None at first but this is where we will store the reversed LL
+    # the curr pointer will be initialized with the head reference
     prev, curr = None, head
-
+    # We'll iterate through the LL until we get to a null(i.e. end of the LL)
     while curr:
-        tmp = curr.next
+        # We need to save the next node in order to move to the next iteration of the loop after we modify the references
+        nextNode = curr.next
+        # Let's reverse the links of our current node. We need to update the next reference of node to point to prev
         curr.next = prev
+        # Now we want to move further in our iteration, so the current node will become previous node, so we should set prev to curr
         prev = curr
-        curr = tmp
+        # Lastly, the nextNode will become the current node, so we should update curr to nextNode
+        curr = nextNode
     return prev
