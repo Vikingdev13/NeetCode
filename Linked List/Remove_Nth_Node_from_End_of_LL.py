9cf5f8cc2b2b4983c03166c699d84b7d3d605ed7
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-06-08 15:30:22 -0400

    Update Remove_Nth_Node_from_End_of_LL.py

--- Linked List/Remove_Nth_Node_from_End_of_LL.py
+++ Linked List/Remove_Nth_Node_from_End_of_LL.py
@@ -10,19 +10,24 @@ Space: O(1)
 #         self.val = val
 #         self.next = next
 
-def removeNthFromEnd(head, n):    
-    dummy = ListNode(0, head)
-    left, right = dummy, head
+def removeNthFromEnd(head, n):
+    # initalize a dummy node to handle edge cases like if the LL has only 1 node or empty and 2 pointers
+    dummy = fast = slow = ListNode(0, next=head)
 
-    while n > 0 and right:
-        right = right.next
-        n -= 1
+    # move the fast pointer to the len of n down the LL
+    for _ in range(n):
+        fast = fast.next
     
-    while right:
-        left = left.next
-        right = right.next
-
-    left.next = left.next.next
+    # while the fast pointer hasnt reached a null
+    while fast.next:
+        # iterate both pointers at the same time, 
+        # but fast will be n nodes away from slow pointer
+        slow = slow.next
+        fast = fast.next
+    # now the slow pointer should be at the node just BEFORE the node that needs removed, 
+    # so take that nodes next pointer and connect it to the node AFTER the node that is being removed
+    slow.next = slow.next.next
+    # return the new list
     return dummy.next
 
 head = [1,2,3,4,5]
