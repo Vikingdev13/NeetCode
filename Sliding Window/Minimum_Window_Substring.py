6fb8ce00fb3aaf46a337707109e48eade53226fd
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-06-10 14:17:09 -0400

    Update Minimum_Window_Substring.py

--- Sliding Window/Minimum_Window_Substring.py
+++ Sliding Window/Minimum_Window_Substring.py
@@ -9,61 +9,42 @@ A substring is a contiguous sequence of characters within the string
 Time: O(n+m)
 Space: O(n)
 """
-from collections import Counter
-
 def get_minimum_window(original, check):
-    # Counts the number of each character of "check"
-    check_count = Counter(check)
-    # Counts the number of each character in the sliding window
-    window_count = {}
-    # Count the number of entries in "check_count" that is smaller than or equal to
-    # that in "window_count"
-    # If "satisfy_count" is equal to the number of entries in "check_count",
-    # that window contains "check". We then just need to check if its the minimum.
-    satisfy_count = 0
-    original_len = len(original)
-    # Two pointers pointing to the window (inclusive start, exclusive end)
-    start_ptr = 0
-    end_ptr = 0
-    # The number of entries in "check_count". Used to check if "window_count" contains
-    # "check_count"
-    match_req = len(check_count.keys())
-    # The smallest recorded string that satisfies the conditions.
-    smallest_str = None
-    # Change the number of "char" inside the window by "delta"
-    # Automatically increase or decrease "satisfy_count" to reflect the current value.
-    def delta_char(char, delta):
-        nonlocal satisfy_count
-        if char not in window_count:
-            window_count[char] = 0
-        if window_count[char] >= check_count.get(char, 0):
-            satisfy_count -= 1
-        window_count[char] += delta
-        if window_count[char] >= check_count.get(char, 0):
-            satisfy_count += 1
-    while end_ptr < original_len:
-        # Moves the end pointer until it contains "check" or it reaches the end
-        while end_ptr < original_len and satisfy_count < match_req:
-            delta_char(original[end_ptr], 1)
-            end_ptr += 1
-        # If the window reaches the end and does not contain "check", break loop
-        if end_ptr == original_len and satisfy_count < match_req:
-            break
-        # Otherwise, the window contains "check", so we move the start pointer
-        # until it no longer does. Then, the one before failing the check is the local
-        # minimal substring.
-        while satisfy_count >= match_req:
-            delta_char(original[start_ptr], -1)
-            start_ptr += 1
-        valid_window = original[start_ptr - 1 : end_ptr]
-        # Compare the local minimum to the stored smallest string
-        # If there is nothing stored, or the condition outlined is true, we store the string
-        if smallest_str is None or (len(smallest_str) > len(valid_window)):
-            smallest_str = valid_window
-        elif len(smallest_str) == len(valid_window) and valid_window < smallest_str:
-            smallest_str = valid_window
-    return smallest_str or ""
+    left, matched, substr_start = 0, 0, 0
+    min_length = len(original) + 1
+    charFreq = {}
 
+    for chr in check:
+        if chr not in charFreq:
+            charFreq[chr] = 1 + charFreq.get(chr, 0)
+        
+
+    # try to extend the range [left, right]
+    for right in range(len(original)):
+        right_char = original[right]
+        if right_char in charFreq:
+            charFreq[right_char] -= 1
+            if charFreq[right_char] >= 0:  # Count every matching of a character
+                matched += 1
+
+        # Shrink the window if we can, finish as soon as we remove a matched character
+        while matched == len(check):
+            if min_length > right - left + 1:
+                min_length = right - left + 1
+                substr_start = left
+
+            left_char = original[left]
+            left += 1
+            if left_char in charFreq:
+                # Note that we could have redundant matching characters, therefore we'll decrement the
+                # matched count only when a useful occurrence of a matched character is going out of the window
+                if charFreq[left_char] == 0:
+                    matched -= 1
+                charFreq[left_char] += 1
+
+    if min_length > len(original):
+        return ""
+    return original[substr_start:substr_start + min_length]
 
 original = "ADOBECODEBANC"
 check = "ABC"
