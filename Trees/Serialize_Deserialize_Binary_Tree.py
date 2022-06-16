e4c21eec6552a5f338057d406761f7f780ec00f4
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-06-16 12:10:06 -0400

    Create Serialize_Deserialize_Binary_Tree.py

---
+++ Trees/Serialize_Deserialize_Binary_Tree.py
@@ -0,0 +1,40 @@
+"""
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+"""
+"""
+Time: 
+Space:  
+"""
+class Codec:
+
+    def serialize(root):
+        result = []
+
+        def dfs(node):
+            if not node:
+                result.append("N")
+                return
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(result)
+
+    def deserialize(data):
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
