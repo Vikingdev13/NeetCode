4e9648812b835b24bfd609736da90864141b835f
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-08-31 16:37:34 -0400

    Update Group_Anagrams.py

--- Arrays and Hashing/Group_Anagrams.py
+++ Arrays and Hashing/Group_Anagrams.py
@@ -38,44 +38,44 @@ print(groupAnagrams(strings))
 Time: O(nmlog(m))
 Space: O(n*m)
 """
-# def groupAnagrams2(strings):
-#     results = {}
-#     for i in strings:
-#         x = "".join(sorted(i))
-#         if x in results:
-#             results[x].append(i)
-#         else:
-#             results[x] = [i]
-#     return list(results.values())
+def groupAnagrams2(strings):
+    results = {}
+    for i in strings:
+        x = "".join(sorted(i))
+        if x in results:
+            results[x].append(i)
+        else:
+            results[x] = [i]
+    return list(results.values())
 
-# strings = ["eat","tea","tan","ate","nat","bat"]
-# print(groupAnagrams2(strings))
+strings = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams2(strings))
 
-# """
-# Time: O(m*n) - where m is the total input strings given and n is the avg length of those strings
-# Space: O(n*m)
-# """
-# def groupAnagrams3(strings):
-#     # initialize an empty hashMap to be used to map character count to list of anagrams
-#     # the following example sets the key as a tuple since in python lists cant be keys
-#     # bc they're mutable, but the key is a tuple of character counts, and the values are the
-#     # lists of the words that can be created using only those characters
-#     # EX: key = (1:a,1:n,1:t) : ['ant','tan','nat']
-#     result = collections.defaultdict(list)
-#     # iterate through the input string    
-#     for string in strings:
-#         # count = 26 zeros to start, 1 for each character of the alphabet
-#         count = [0] * 26
-#         # iterate through every character of each input string
-#         for ch in string:
-#             # count the characters and map the characters to numbers 0-25
-#             # a=0....z=25
-#             # take ASCII val of current character and subract it from lowercase a, then increment that by 1
-#             count[ord(ch) - ord('a')] += 1
-#         # group all strings with same count together and append those strings to the output array
-#         result[tuple(count)].append(string)
-#     return list(result.values())
+"""
+Time: O(m*n) - where m is the total input strings given and n is the avg length of those strings
+Space: O(n*m)
+"""
+def groupAnagrams3(strings):
+    # initialize an empty hashMap to be used to map character count to list of anagrams
+    # the following example sets the key as a tuple since in python lists cant be keys
+    # bc they're mutable, but the key is a tuple of character counts, and the values are the
+    # lists of the words that can be created using only those characters
+    # EX: key = (1:a,1:n,1:t) : ['ant','tan','nat']
+    result = collections.defaultdict(list)
+    # iterate through the input string    
+    for string in strings:
+        # count = 26 zeros to start, 1 for each character of the alphabet
+        count = [0] * 26
+        # iterate through every character of each input string
+        for ch in string:
+            # count the characters and map the characters to numbers 0-25
+            # a=0....z=25
+            # take ASCII val of current character and subract it from lowercase a, then increment that by 1
+            count[ord(ch) - ord('a')] += 1
+        # group all strings with same count together and append those strings to the output array
+        result[tuple(count)].append(string)
+    return list(result.values())
 
 
-# strings = ["eat","tea","tan","ate","nat","bat"]
-# print(groupAnagrams3(strings))
+strings = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams3(strings))
