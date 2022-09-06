41520cb516b17b22b85e707678d8bf0575a4abea
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-09-06 14:33:18 -0400

    Create LRU_Cache.py

---
+++ Linked List/LRU_Cache.py
@@ -0,0 +1,72 @@
+"""
+Design a data structure that follows the constraints of a left Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+"""
+"""
+Time: O(1)
+Space: O(n)
+"""
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        # left = lru, right = most recently used
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    # Helper functions
+    # remove node from list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    # insert node at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.next = node
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+obj = LRUCache(2)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+obj.put(1, 1); # cache is {1=1}
+obj.put(2, 2); # cache is {1=1, 2=2}
+obj.get(1);    # return 1
+obj.put(3, 3); # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+obj.get(2);    # returns -1 (not found)
+obj.put(4, 4); # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+obj.get(1);    # return -1 (not found)
+obj.get(3);    # return 3
+obj.get(4);    # return 4
