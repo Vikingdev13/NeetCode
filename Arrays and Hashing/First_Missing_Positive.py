ac6efb348579cf36e89f788803177e26c3ed652c
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-09-08 14:48:39 -0400

    Create First_Missing_Positive.py

---
+++ Arrays and Hashing/First_Missing_Positive.py
@@ -0,0 +1,19 @@
+"""
+Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+"""
+"""
+Time: O(n)
+Space: O(n) - couldn't solve in constant space
+"""
+def firstMissingPositive(nums):
+    n = len(nums)
+    hashSet = set(nums)
+
+    for i in range(1, n+2):
+        if i not in hashSet:
+            return i
+
+nums = [1,2,6,4,3,1]
+print(firstMissingPositive(nums))
