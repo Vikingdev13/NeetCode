f7a58bb1cefd5b87ce90f0bb863329302461a1b7
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-09-13 16:08:39 -0400

    Create Kth Largest Element in a Stream.py

---
+++ Heap:Priority Q/Kth Largest Element in a Stream.py
@@ -0,0 +1,29 @@
+"""
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+"""
+"""
+Time: 
+Space: 
+"""
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        # min heap with k largest integers
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        # in min heaps, the min val is always stored in heap[0]
+        return self.minHeap[0]
+
