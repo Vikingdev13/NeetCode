8b8727ea99425f61cea4cdd4845cbf579ee55f6e
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-09-26 14:16:16 -0400

    Update LRU_Cache.py

--- Linked List/LRU_Cache.py
+++ Linked List/LRU_Cache.py
@@ -56,17 +56,25 @@ class LRUCache:
             self.remove(lru)
             del self.cache[lru.key]
 
-# Your LRUCache object will be instantiated and called as such:
-obj = LRUCache(2)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+"""
+def __init__(self, capacity):
+    self.dic = collections.OrderedDict()
+    self.remain = capacity
 
-obj.put(1, 1); # cache is {1=1}
-obj.put(2, 2); # cache is {1=1, 2=2}
-obj.get(1);    # return 1
-obj.put(3, 3); # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-obj.get(2);    # returns -1 (not found)
-obj.put(4, 4); # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-obj.get(1);    # return -1 (not found)
-obj.get(3);    # return 3
-obj.get(4);    # return 4
+def get(self, key):
+    if key not in self.dic:
+        return -1
+    v = self.dic.pop(key) 
+    self.dic[key] = v   # set key as the newest one
+    return v
+
+def set(self, key, value):
+    if key in self.dic:    
+        self.dic.pop(key)
+    else:
+        if self.remain > 0:
+            self.remain -= 1  
+        else:  # self.dic is full
+            self.dic.popitem(last=False) 
+    self.dic[key] = value
+"""
