d12dd92b4de3609dcc47cdaf41d4625da6e86895
Vikingdev13 <vikingdeveloper13@gmail.com>
2022-09-28 15:07:23 -0400

    Update Balanced_Binary_Tree.py

--- Trees/Balanced_Binary_Tree.py
+++ Trees/Balanced_Binary_Tree.py
@@ -17,7 +17,10 @@ def isBalanced(root):
         if not root:
             return [True, 0]
         left, right = dfs(root.left), dfs(root.right)
+        # balanced is true when the left and right subtrees are balanced and the 
+        # abs of the left and right heights after the root
         balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
 
         return [balanced, 1 + max(left[1], right[1])]
+    # the inner function returns 2 values, but we only want the first value of that return statement, which is the boolean value
     return dfs(root)[0]
